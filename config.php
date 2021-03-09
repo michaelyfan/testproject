31f78b3f01aaa3ee0d465a72005804bfddf82c7b
Michael Fan <michaelyfan100@gmail.com>
2021-03-09 12:06:03 -0500

    test for deploy

--- config.php
+++ config.php
@@ -13,6 +13,7 @@ $password   = $_ENV["PASSWORD"];
 $dbname     = $_ENV["DBNAME"];
 $port       = $_ENV["PORT"];
 $dsn        = "mysql:host=$host;dbname=$dbname;port=$port";
+echo $host;
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
