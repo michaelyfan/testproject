367c238344b200657ce0da90131e814dba88648f
Michael Fan <michaelyfan100@gmail.com>
2021-03-10 15:58:29 -0500

    Change env var retrieval based on environment

--- config.php
+++ config.php
@@ -2,16 +2,28 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-if (!(isset($_ENV["PRODUCTION"]) && $_ENV["PRODUCTION"]=="true")) {
+$host       = "";
+$username   = "";
+$password   = "";
+$dbname     = "";
+$port       = "";
+
+if (getenv("PRODUCTION")=="true") {
+  $host       = getenv("HOST");
+  $username   = getenv("USERNAME");
+  $password   = getenv("PASSWORD");
+  $dbname     = getenv("DBNAME");
+  $port       = getenv("PORT");
+} else {
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
   $dotenv->load();
+  $host       = $_ENV["HOST"];
+  $username   = $_ENV["USERNAME"];
+  $password   = $_ENV["PASSWORD"];
+  $dbname     = $_ENV["DBNAME"];
+  $port       = $_ENV["PORT"];
 }
 
-$host       = $_ENV["HOST"];
-$username   = $_ENV["USERNAME"];
-$password   = $_ENV["PASSWORD"];
-$dbname     = $_ENV["DBNAME"];
-$port       = $_ENV["PORT"];
 $dsn        = "mysql:host=$host;dbname=$dbname;port=$port";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
